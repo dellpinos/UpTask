66bc908548c24cdd84a015e0d4fce28d81341466
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-05-13 18:50:46 -0300

    CSS a ventana Modal

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -10,11 +10,17 @@ class DashboardController {
     public static function index(Router $router) {
 
         session_start();
-
         isAuth();
+        
+        $id = $_SESSION['id'];
+
+        $proyectos = Proyecto::belongsTo('propietarioId', $id);
+
+
 
         $router->render('dashboard/index', [
-            'titulo' => 'Proyectos'
+            'titulo' => 'Proyectos',
+            'proyectos' => $proyectos
         ]);
     }
     public static function crear_proyecto(Router $router){
--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,4 +1,22 @@
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
 
+<?php if (count($proyectos) === 0) { ?>
+    <p class="no-proyectos">No hay proyectos aún.
+        <a href="/crear-proyecto">Comienza a crear proyectos !</a>
+    </p>
+<?php } else { ?>
+    <ul class="listado-proyectos">
+        <?php foreach ($proyectos as $proyecto) : ?>
+
+            <a href="/proyecto?id=<?php echo $proyecto->url ?>">
+                <li class="proyecto">
+                    <p><?php echo $proyecto->proyecto; ?></p>
+                </li>
+            </a>
+
+        <?php endforeach; ?>
+    </ul>
+
+<?php } ?>
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
