28f6546e7de71cfd70cde88997926fab30e0b66f
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-05-27 20:30:59 -0300

    Cambiando METHOD por URI en el Router

--- Router.php
+++ Router.php
@@ -20,8 +20,8 @@ class Router
     public function comprobarRutas()
     {
 
-        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
-        $method = $_SERVER['REQUEST_URI'];
+        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_METHOD'];
+        $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
             $fn = $this->getRoutes[$currentUrl] ?? null;
