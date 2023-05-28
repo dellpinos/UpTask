b50db966a9c14995ca3239eb72955f21569cdea1
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-05-27 21:08:12 -0300

    Modificando DB

--- Router.php
+++ Router.php
@@ -19,10 +19,11 @@ class Router
 
     public function comprobarRutas()
     {
-        session_start();
 
         $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
-        $method = $_SERVER['REQUEST_URI'];
+
+        $method = $_SERVER['REQUEST_METHOD'];
+
 
         if ($method === 'GET') {
             $fn = $this->getRoutes[$currentUrl] ?? null;
