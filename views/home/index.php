18a242388f77583f8805d5ce32eb5440fbf285d7
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-06-02 15:41:00 -0300

    Primeros pasos de la homepage, listo para prod

--- views/home/index.php
+++ views/home/index.php
@@ -20,10 +20,9 @@
     <div class="home__contenedor-tareas">
         <div class="home__imagen-tarea"></div>
         <div class="home__texto-tarea">
-            <p>
+            <p class="home__parrafo-tarea">
                 Administra proyectos y tareas de manera eficiente con nuestra plataforma. Con TaskTrack, puedes crear proyectos y asignar tareas de forma rápida y sencilla. Organiza tus proyectos según tus necesidades y mantén un seguimiento claro de cada tarea. Marca las tareas como completadas a medida que avanzas y registra tus avances para tener un registro detallado. Nuestra herramienta simplifica tu flujo de trabajo al ofrecerte una visión clara de tus objetivos y las tareas pendientes.
             </p>
-            
         </div>
     </div>
 
@@ -32,7 +31,7 @@
     <div class="home__contenedor-proyectos">
         <div class="home__texto-proyecto">
             <h3 class="home__heading">Tus proyectos donde quieras</h3>
-            <p>
+            <p class="home__parrafo-proyecto">
                 Nuestra aplicación en línea te permite acceder y gestionar tus proyectos desde cualquier dispositivo con conexión a Internet. Mantén tus tareas organizadas, asigna responsabilidades y realiza un seguimiento del progreso estés donde estés. Con TaskTrack, la movilidad y la flexibilidad son fundamentales para que puedas mantener el control de tus proyectos sin importar dónde te encuentres. Simplifica tu gestión de proyectos y optimiza tu productividad.
             </p>
             <div class="home__enlace-proyecto">
