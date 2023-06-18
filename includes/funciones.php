5e4574b363e23bbffb64b9a4e39ce89de5766bf8
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-06-18 01:58:47 -0300

    Aplicando header() para CORS

--- includes/funciones.php
+++ includes/funciones.php
@@ -1,6 +1,7 @@
 <?php
 
-function debuguear($variable) : string {
+function debuguear($variable): string
+{
     echo "<pre>";
     var_dump($variable);
     echo "</pre>";
@@ -8,14 +9,32 @@ function debuguear($variable) : string {
 }
 
 // Escapa / Sanitizar el HTML
-function s($html) : string {
+function s($html): string
+{
     $s = htmlspecialchars($html);
     return $s;
 }
 
 // Función que revisa que el usuario este autenticado
-function isAuth() : void {
-    if(!isset($_SESSION['login'])) {
+function isAuth(): void
+{
+    if (!isset($_SESSION['login'])) {
         header('Location: /');
+    }
+}
+
+function requiereCors()
+{
+    // Obtener el origen de la solicitud
+    $origin = $_SERVER['HTTP_HOST'];
+
+    // Definir el origen permitido
+    $allowedOrigin = $_ENV['HOST'];
+
+    // Verificar si se requiere el encabezado 'Access-Control-Allow-Origin'
+    $seRequiereCors = $origin !== $allowedOrigin;
+
+    if ($seRequiereCors) {
+        header('Access-Control-Allow-Origin:  ' . $_ENV['HOST']);
     }
 }
